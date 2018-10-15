5b53edc6b3fa292a02e3f4838736300cea1c665a
Beth Johnson <bethjohnson3@gmail.com>
2018-10-15 12:59:57 -0700

    add database and table definition

--- schema.sql
+++ schema.sql
@@ -1,1 +1,13 @@
-
+-- create database grocery_list (drop if exists)
+DROP DATABASE IF EXISTS grocery_list;
+CREATE database grocery_list;
+-- use grocery_list database
+USE grocery_list;
+-- create table with cols id, item, quantity
+CREATE TABLE groceries (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  item TEXT,
+  quantity INT
+);
+-- insert three test values
+INSERT INTO groceries (item, quantity) VALUES ("pizza", 5), ("cheese", 1), ("ice cream", 10);
